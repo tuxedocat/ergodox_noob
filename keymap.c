c9292e4d9ec6a27d8ad3deafb37a37ded34dbba4
tuxedocat <tuxedocat@users.noreply.github.com>
2020-08-28 23:24:37 +0900

    make ctrl+[ easier for vimmer

--- keymap.c
+++ keymap.c
@@ -38,29 +38,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |  Esc~  |   1  |   2  |   3  |   4  |   5  |   6  |           |   7  |   8  |   9  |   0  |   -  |   =  |   BS   |
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |   Tab  |   Q  |   W  |   E  |   R  |   T  | Home |           | PgUp |   Y  |   U  |   I  |   O  |   P  |    \   |
+ * |  Tab   |   Q  |   W  |   E  |   R  |   T  | Home |           | PgUp |   Y  |   U  |   I  |   O  |   P  |    [   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  LCtl  |A/LCtl|   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |;/RCtl|    '   |
+ * |  LCtl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |    '   |
  * |--------+------+------+------+------+------|  End |           | PgDn |------+------+------+------+------+--------|
  * |  LSft  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |  RSft  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | ~LSys|      |      | ~LNav| LSft |                                       |~LCode| ~LNav|      |      |      |
+ *   | ~LSys|      |      | ~LNav|~LCode|                                       |~LCode| ~LNav|      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------+------+------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 | Space| 英数 |------|       |------| かな | Enter|
+ *                                 | Space| 英数 |------|       |------| かな | Enter |
  *                                 |      | /LGui|App/Alt|      | RAlt | /RGui|      |
  *                                 `--------------------'       `--------------------'
  */
 [L_BASE] = LAYOUT_ergodox_pretty(
         // left hand																							right hand
         C_ESC_TILDE,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,				/* */	KC_7,			KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
-        KC_TAB,         KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_HOME,			/* */	KC_PGUP,        KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLS,
-        KC_LCTL, LCTL_T(KC_A),      KC_S,       KC_D,       KC_F,       KC_G,							/* */	KC_H,			KC_J,       KC_K,       KC_L,       RCTL_T(KC_SCLN),        KC_QUOT,
+        KC_TAB,         KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_HOME,			/* */	KC_PGUP,        KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,
+        KC_LCTL,        KC_A,       KC_S,       KC_D,       KC_F,       KC_G,							/* */	                KC_H,		KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
         KC_LSFT,		KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_END,				/* */	KC_PGDN,        KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
-        MO(L_SYS),      XXXXXXX,    XXXXXXX,    MO(L_NAV),  KC_LSFT,									/* */	MO(L_CODE),		MO(L_NAV),  XXXXXXX,    XXXXXXX,    XXXXXXX,
+        MO(L_SYS),      XXXXXXX,    XXXXXXX,    MO(L_NAV),  MO(L_CODE),									/* */	MO(L_CODE),		MO(L_NAV),  XXXXXXX,    XXXXXXX,    XXXXXXX,
                                                                         XXXXXXX,	XXXXXXX,			/* */	XXXXXXX,        XXXXXXX,
 																					XXXXXXX,			/* */	XXXXXXX,
 																KC_SPC, LGUI_T(KC_LANG2),   LALT_T(KC_APP),	/* */	KC_RALT,        RGUI_T(KC_LANG1),   KC_ENT
@@ -71,9 +71,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |    `   |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  |  F10 |  F11 | F12  |   Del  |
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |        |  F11 |  F12 |  F13 |  F14 |  F15 |      |           |      |      |   {  |   }  |   _  |   +  |        |
+ * |        |  F11 |  F12 |  F13 |  F14 |  F15 |      |           |      |      |      |      |   [  |   ]  |    \   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |------|           |------|      |   [  |   ]  |   -  |   =  |        |
+ * |        |      |      |      |      |      |------|           |------|      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -89,9 +89,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [L_CODE] = LAYOUT_ergodox_pretty(
        // left hand																				right hand
-       KC_GRV,  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,		/* */	KC_F7,   KC_F8,      KC_F9,      KC_F10,      KC_F11,      KC_F12,     KC_DEL,
-       _______, KC_F11,     KC_F12,     KC_F13,     KC_F14,     KC_F15,     _______,	/* */	_______, XXXXXXX,    S(KC_LBRC), S(KC_RBRC), S(KC_MINS), S(KC_EQL),  XXXXXXX,
-       _______, _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,				/* */			 XXXXXXX,    KC_LBRC,    KC_RBRC,    KC_MINS,    KC_EQL,     XXXXXXX,
+       KC_GRV,  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,		/* */	KC_F7,   KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,
+       _______, KC_F11,     KC_F12,     KC_F13,     KC_F14,     KC_F15,     _______,	/* */	_______, XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_LBRC,    KC_RBRC,    KC_BSLS,
+       _______, _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,				/* */			 XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
        _______, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,	/* */	_______, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
        XXXXXXX, XXXXXXX,    XXXXXXX,    _______,    _______,							/* */						 _______,    _______,    _______,    _______,    _______,
                                                                 _______,    _______,	/* */	_______, _______,
@@ -330,6 +330,7 @@ static void fixed_update(void) {
     last_layer      = layer;
 
     return;
+}
 
 
 
